6a47fcee1d079b7cce0fe401cbca72d5be004c8a
itsrealmj <88704692+itsrealmj@users.noreply.github.com>
2021-12-05 07:59:47 +0800

    sample

--- js/app.js
+++ js/app.js
@@ -2,27 +2,44 @@ let cartImageBtn = document.querySelector('#cart-img');
 let cartModal = document.querySelector('.cart-modal'); 
 
     cartImageBtn.addEventListener('click', function(){
-        // let cartModalBg = document.querySelector('.cart-modal-bg');
         cartModal.classList.add('cart-modal-active');
-        
+        document.querySelector('#trash').addEventListener('click', function(){
+            cartModal.classList.remove('cart-modal-active');
+        })
     })
-     
-const menu = document.querySelector('#menu');
-        menu.addEventListener('click', function(){
-            const navUL = document.querySelector('.nav-ul');
-            navUL.style.transition = '1s';
-            navUL.classList.add('nav-activate');
 
-            
-            const closedNav = document.querySelector('.closed-nav')
-                closedNav.addEventListener('click', function(){
-                navUL.classList.remove('nav-activate');
-                })
+
+    function myCart(photo,date){
+        return `This ${photo} photo  was taken on the year ${date}`;
+    }
+    
+
+    let addToCartBtn = document.querySelector('.btn');
+        addToCartBtn.addEventListener('click', function(){
+            document.querySelector('.details').textContent =  myCart('happy family', 2022) ;
+            document.querySelector('#sudlanan').src = tinyImages[3]
+           
+
         })
 
 
 
-
+let xxx = document.querySelector('#xxx')
+let menu = document.querySelector('#menu');
+        menu.addEventListener('click', function(){
+            const navUL = document.querySelector('.nav-ul');
+            navUL.classList.add('nav-activate'); 
+            menu.style.display = "none";
+            xxx.style = "display:block; margin-right:1rem;"
+            
+        })
+        xxx.addEventListener('click', function(){
+            const navUL = document.querySelector('.nav-ul');
+            navUL.classList.remove('nav-activate'); 
+            menu.style.display = "block";
+            xxx.style.display = "none";
+            
+        })
 
 
 
@@ -37,4 +54,35 @@ for (let index = 0; index < small.length; index++) {
     
 }
     
-    
+let tinyImages = [
+    './images/image-product-1.jpg',
+    './images/image-product-2.jpg',
+    './images/image-product-3.jpg',
+    './images/image-product-4.jpg'
+]; 
+
+
+let prev = document.querySelector('#prev');
+let next = document.querySelector('#next');
+
+let currentPic = 1;
+next.addEventListener('click', function(){
+    displayImage.src = tinyImages[currentPic];
+    currentPic++;
+    if (currentPic > tinyImages.length -1) {
+        currentPic = 0;        
+    }
+})
+prev.addEventListener('click', function(){
+    displayImage.src = tinyImages[currentPic];
+    currentPic--;
+
+    if (currentPic < 0) {
+        currentPic = tinyImages.length -1
+    }
+})
+
+
+
+
+
